cc9364b9f1cbc43b0d9af81bff1d32aceaebc17c
augustomont <augustoengenharia@outlook.com>
2023-10-30 18:20:12 -0300

    botao editar funcional refatoracao botao novo, botao cancelar refatoracao de varias outras partes inclusao de verificacao de cpf inclusao de verificacao de imagem

--- sistema-caixa-pdv/cadastro/frmFuncionario.Designer.cs
+++ sistema-caixa-pdv/cadastro/frmFuncionario.Designer.cs
@@ -191,6 +191,7 @@
             this.btnEditar.TabIndex = 15;
             this.btnEditar.Text = "Editar";
             this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
             // 
             // btnExcluir
             // 
--- sistema-caixa-pdv/cadastro/frmFuncionario.cs
+++ sistema-caixa-pdv/cadastro/frmFuncionario.cs
@@ -15,10 +15,16 @@ namespace sistema_caixa_pdv.cadastro
 {
     public partial class frmFuncionario : Form
     {
+        //variaveis globais de referencia
         Conexao conexao = new Conexao();
         string sql;
         MySqlCommand cmd;
         string foto;
+        string id;
+        string cpfAntigo;
+        bool fotoAlterada = false;
+
+
         public frmFuncionario()
         {
             InitializeComponent();
@@ -27,6 +33,7 @@ namespace sistema_caixa_pdv.cadastro
         {
             LimparFoto();
             grid.Rows.Clear();//limpar o grid antes de preencheer
+            fotoAlterada = false;
             Listar();
         }
         private void btnNovo_Click(object sender, EventArgs e)
@@ -34,7 +41,8 @@ namespace sistema_caixa_pdv.cadastro
             LimparCampos();
             LimparFoto();
             HabilitarCampos();
-            txtNome.Focus();
+            grid.Enabled = false;
+
         }
         private void grid_CellClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -42,18 +50,7 @@ namespace sistema_caixa_pdv.cadastro
             {
                 HabilitarEdicao();
                 PreencherCampos();
-
-                //preencher foto separadamente
-                if (grid.CurrentRow.Cells[7].Value != DBNull.Value) //Verific se tem foto salva
-                {
-                    byte[] imagem = (byte[])grid.Rows[e.RowIndex].Cells[7].Value; //Criar array bytes[] imagem para receber a foto da tabela em bytes
-                    MemoryStream ms = new MemoryStream(imagem); //recebe o array byte[] ja com o valor convertido da foto
-                    imgFoto.Image = Image.FromStream(ms); //passando o memoryStream no objeto que ele recebe um System.Drawing e seu parameter FromStream que vai receber
-                }
-                else
-                {
-                    imgFoto.Image = Properties.Resources.pessoa; //aqui insere a foto padrãodo sistema
-                }
+                PreencherFoto(e);//recebe argumento para verificar se o campo esta vazio
             }
             else
             {
@@ -65,67 +62,57 @@ namespace sistema_caixa_pdv.cadastro
         {
             if (e.RowIndex > -1)
             {
-                HabilitarCampos();
                 PreencherCampos();
+                HabilitarCampos();
                 HabilitarEdicao();
                 
+                grid.Enabled = false;
             }
         }
         private void btnCancelar_Click(object sender, EventArgs e)
         {
-            LimparCampos();
-            LimparFoto();
-            DesabilitarCampos();
-            HabilitarNovo();
+            Resetar();
         }
 
         private void btnSalvar_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(txtNome.Text.Trim()))// trim para verificar se tem espaços vazios
+            if(VerificarCamposObrigatorios())
             {
-                MessageBox.Show("Preencha o nome!", "Cadastro Funcionários", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                txtNome.Text = string.Empty;
-                txtNome.Focus();
-                return;
-            }
-            else if (txtCPF.Text == "   ,   .   -" || txtCPF.Text.Trim().Length < 14)
-            {
-                MessageBox.Show("Preencha o CPF!", "Cadastro Funcionaarios", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                txtCPF.Text = string.Empty;
-                txtCPF.Focus();
-                return;
-            }
-            else if (txtTelefone.Text == "(  )      -" || txtTelefone.Text.Trim().Length < 10)
-            {
-                MessageBox.Show("Preencha o telefone!", "Cadastro Funcionarios", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                txtTelefone.Text = string.Empty;
-                txtTelefone.Focus();
-                return;
-            }
-
-            conexao.AbrirConexao();
-            sql = "INSERT INTO funcionarios(nome, cpf, telefone, endereco, cargo, data, foto)" +
-                "VALUES (@nome, @cpf, @telefone, @endereco, @cargo, curDate(), @foto)";
+                conexao.AbrirConexao();
+                sql = "INSERT INTO funcionarios(nome, cpf, telefone, endereco, cargo, data, foto)" +
+                    "VALUES (@nome, @cpf, @telefone, @endereco, @cargo, curDate(), @foto)";
             
-            cmd = new MySqlCommand(sql, conexao.conexao);
+                cmd = new MySqlCommand(sql, conexao.conexao);
 
-            cmd.Parameters.Clear();
-            cmd.Parameters.AddWithValue("@nome", txtNome.Text);
-            cmd.Parameters.AddWithValue("@cpf", txtCPF.Text);
-            cmd.Parameters.AddWithValue("@telefone", txtTelefone.Text);
-            cmd.Parameters.AddWithValue("@endereco", txtEndereco.Text);
-            cmd.Parameters.AddWithValue("@cargo", cbCargo.Text);
-            cmd.Parameters.AddWithValue("@foto", Img());//img é um metodo criado por mim para tratar imagem para o banco de dados
+                cmd.Parameters.Clear();
 
-            cmd.ExecuteNonQuery();
+                AdicionarValoresSql();//adiciona nome, cpf, telefone, endereco, cargo
+                cmd.Parameters.AddWithValue("@foto", Img());//img é um metodo para tratar imagem para o banco de dados. Imagem precisa estar em formato de Array de Bytes
+
+                cmd.ExecuteNonQuery();
             
-            conexao.FecharConexao();
+                conexao.FecharConexao();
 
-            LimparFoto();
-            LimparCampos();
-            DesabilitarCampos();
-            Listar();
+                MessageBox.Show("Cadastro Salvo!", "Cadastro Funcionarios", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Resetar();
+            }
+        }
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if(VerificarCamposObrigatorios())
+            {
+                conexao.AbrirConexao();
 
+                VerificarImagemAlteradaESalvar();//Salva alteriacoes feitas
+                VerificarCpfExiste();
+
+                cmd.ExecuteNonQuery();
+                conexao.FecharConexao();
+                Listar();
+
+                MessageBox.Show("Registro Editado com sucesso!", "Cadastro Funcionarios", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Resetar();
+            }
         }
 
         private void btnFoto_Click(object sender, EventArgs e)
@@ -139,6 +126,7 @@ namespace sistema_caixa_pdv.cadastro
                 foto = dialog.FileName.ToString();//pegando o caminho da imagem que foi selecionada e dado OK
                 imgFoto.ImageLocation = foto; //jogando o caminho da imagem para o componente imgFoto para exibir no form
 
+                fotoAlterada = true;//importante informacao para verificar na hora de editar registro
             }
         }
 
@@ -163,6 +151,19 @@ namespace sistema_caixa_pdv.cadastro
             imgFoto.Image = Properties.Resources.pessoa;// aqui coloca a imagem pessoa.png na picture do form
             foto = "img/pessoa.png"; //atribuindo um caminho de foto (esssa imagem te que estar na pasta debug)
         }
+        private void PreencherFoto(DataGridViewCellEventArgs e)
+        {
+            if (grid.CurrentRow.Cells[7].Value != DBNull.Value) //Verific se tem foto salva
+            { 
+                byte[] imagem = (byte[])grid.Rows[e.RowIndex].Cells[7].Value; //Criar array bytes[] imagem para receber a foto da tabela em bytes
+                MemoryStream ms = new MemoryStream(imagem); //recebe o array byte[] ja com o valor convertido da foto
+                imgFoto.Image = Image.FromStream(ms); //passando o memoryStream no objeto que ele recebe um System.Drawing e seu parameter FromStream que vai receber
+            }
+            else
+            {
+                imgFoto.Image = Properties.Resources.pessoa; //aqui insere a foto padrãodo sistema
+            }
+        }
         private void LimparCampos()
         {
             txtNome.Clear();
@@ -182,6 +183,8 @@ namespace sistema_caixa_pdv.cadastro
 
             btnSalvar.Enabled = true;
             btnNovo.Enabled = false;
+
+            txtNome.Focus();
         }
         private void DesabilitarCampos()
         {
@@ -200,6 +203,7 @@ namespace sistema_caixa_pdv.cadastro
             btnSalvar.Enabled = false;
             btnEditar.Enabled = false;
             btnExcluir.Enabled = false;
+
         }
         private void HabilitarEdicao()
         {
@@ -207,6 +211,7 @@ namespace sistema_caixa_pdv.cadastro
             btnSalvar.Enabled=false;
             btnEditar.Enabled = true;
             btnExcluir.Enabled = true;
+
         }
         private void Listar()
         {
@@ -225,8 +230,10 @@ namespace sistema_caixa_pdv.cadastro
         }
         private void PreencherCampos()
         {
+            id = grid.CurrentRow.Cells[0].Value.ToString();
             txtNome.Text = grid.CurrentRow.Cells[1].Value.ToString();
             txtCPF.Text = grid.CurrentRow.Cells[2].Value.ToString();
+            cpfAntigo = grid.CurrentRow.Cells[2].Value.ToString();//importante para posterior verificacao
             txtTelefone.Text = grid.CurrentRow.Cells[3].Value.ToString();
             txtEndereco.Text = grid.CurrentRow.Cells[4].Value.ToString();
             cbCargo.Text = grid.CurrentRow.Cells[5].Value.ToString();
@@ -242,6 +249,87 @@ namespace sistema_caixa_pdv.cadastro
             grid.Columns[5].HeaderText = "Função";
             grid.Columns[6].HeaderText = "Data de Inserção";
         }
-
+        private bool VerificarCamposObrigatorios()
+        {
+            if (string.IsNullOrEmpty(txtNome.Text.Trim()))// trim para verificar se tem espaços vazios
+            {
+                MessageBox.Show("Preencha o nome!", "Cadastro Funcionários", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtNome.Text = string.Empty;
+                txtNome.Focus();
+                return false;
+            }
+            else if (txtCPF.Text == "   ,   .   -" || txtCPF.Text.Trim().Length < 14)
+            {
+                MessageBox.Show("Preencha o CPF!", "Cadastro Funcionaarios", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtCPF.Text = string.Empty;
+                txtCPF.Focus();
+                return false;
+            }
+            else if (txtTelefone.Text == "(  )      -" || txtTelefone.Text.Trim().Length < 10)
+            {
+                MessageBox.Show("Preencha o telefone!", "Cadastro Funcionarios", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtTelefone.Text = string.Empty;
+                txtTelefone.Focus();
+                return false;
+            }
+            return true;
+        }
+        private void VerificarImagemAlteradaESalvar()
+        {
+            if (fotoAlterada == true)
+            {
+                sql = "UPDATE funcionarios SET nome = @nome, cpf = @cpf, telefone = @telefone, endereco = @endereco, cargo = @cargo, foto = @foto " +
+                    "WHERE id = @id";
+                cmd = new MySqlCommand(sql, conexao.conexao);
+                cmd.Parameters.AddWithValue("@id", id);
+                AdicionarValoresSql();
+                cmd.Parameters.AddWithValue("foto", Img());
+            }
+            else if (fotoAlterada == false)
+            {
+                sql = "UPDATE funcionarios SET nome = @nome, cpf = @cpf, telefone = @telefone, endereco = @endereco, cargo = @cargo " +
+                    "WHERE id = @id";
+                cmd = new MySqlCommand(sql, conexao.conexao);
+                cmd.Parameters.AddWithValue("@id", id);
+                AdicionarValoresSql();
+            }
+        }
+        private void VerificarCpfExiste()
+        {
+            if (txtCPF.Text != cpfAntigo)//Verifica se CPF é igual ao que aparece no formulario
+            {
+                MySqlCommand cmdVerificar = new MySqlCommand("Select * FROM funcionarios WHERE cpf = @CPF", conexao.conexao);//faz a consulta ao banco
+                MySqlDataAdapter da = new MySqlDataAdapter(cmdVerificar);// é criado para executar a consulta
+                cmdVerificar.Parameters.AddWithValue("@cpf", txtCPF.Text);
+                DataTable dt = new DataTable();//cria uma nova tabela com essa consulta
+                da.Fill(dt);//preenche a nova tabela com os valores encontrados na consulta
+                if (dt.Rows.Count > 0)//verifica-se se a tabela de dados contém alguma linha. Se houver pelo menos uma linha, significa que um registro com o mesmo CPF já existe no banco de dados.
+                {
+                    MessageBox.Show("CPF ja registrado!", "Cadastro Funcionários", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    txtCPF.Focus();
+                    return;
+                }
+            }
+        }
+        private void AdicionarValoresSql()
+        {
+            //id esta como auto_increment
+            cmd.Parameters.AddWithValue("@nome", txtNome.Text);
+            cmd.Parameters.AddWithValue("@cpf", txtCPF.Text);
+            cmd.Parameters.AddWithValue("@telefone", txtTelefone.Text);
+            cmd.Parameters.AddWithValue("@endereco", txtEndereco.Text);
+            cmd.Parameters.AddWithValue("@cargo", cbCargo.Text);
+            //img() foto são atribuidaas separadamente
+        }
+        private void Resetar()
+        {
+            LimparCampos();
+            LimparFoto();
+            DesabilitarCampos();
+            HabilitarNovo();
+            Listar();
+            fotoAlterada = false;
+            grid.Enabled = true;
+        }
     }
 }
